308b968c5dffe427134df6b9fe9d06137e28d98b
wearenazi123 <wearenazi123@126.com>
2023-03-26 12:21:25 +0800

    main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -23,6 +25,16 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("I am");
+        Random random=new Random(47);
+        float[] f=new float[10];
+        for (int i=0;i<10;i++){
+            f[i]=random.nextFloat();
+
+        }
+
+        for (float x:f){
+            System.out.println(x);
+        }
 
     }
 }
